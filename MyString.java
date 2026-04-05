a24bf2628e615e65c9be725477019056ab5c11da
KaaranSharmaa <karansharma07111@gmail.com>
2026-04-05 12:35:47 +0530

    Stirng buffer

---
+++ MyString.java
@@ -0,0 +1,8 @@
+class MyString{
+    public static void main (String [] args) {
+        StringBuffer name =new StringBuffer("Karan");
+        name.append(" Sharma");
+        System.out.println(name );
+
+    }
+}
